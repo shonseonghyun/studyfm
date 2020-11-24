6febdb20913fd23cdecc13d157528e8d4f04350a
Seonghyun Son <sunghyun7895@naver.com>
2020-11-24 17:54:42 +0900

    Controller 추가

--- src/main/java/controller/notice/DetailController.java
+++ src/main/java/controller/notice/DetailController.java
@@ -1,4 +1,4 @@
-package controller;
+package controller.notice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,16 +6,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+public class DetailController implements Controller{
 
-	//이것도 요청
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv=new ModelAndView();
-		//이것도 요청
-		mv.addObject("data", "Hello mvc~");
-		mv.setViewName("index");
+		ModelAndView mv = new ModelAndView("notice/detail");
 		return mv;
 	}
-
 }
