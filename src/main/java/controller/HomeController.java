8f9f0ebcc510c65e22e353e5f2c120e90666154c
Seonghyun Son <sunghyun7895@naver.com>
2020-11-25 18:04:16 +0900

    문서출력 4가지 방법 중 @ResponseBody, HttpServletResponse 2개사용

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import entity.Notice;
 import service.NoticeService;
@@ -18,6 +23,13 @@ public class HomeController {
 	
 	@RequestMapping("/index")
 	public String index() {
+//		PrintWriter out;
+//		try {
+//			out = response.getWriter();
+//			out.println("hello");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		return "index";
 	}
 	
